cf512ff8e960d6931e94288ac339ebad0f476942
jihyehan91 <toto5523@hanmail.net>
2023-11-17 11:16:38 +0900

    img 수정

--- js/diary.js
+++ js/diary.js
@@ -3,38 +3,39 @@ document.addEventListener('DOMContentLoaded', function () {
   const inputImgs = document.querySelectorAll('.img');
 
   inputImgs.forEach((inputImg) => {
-    inputImg.addEventListener("input", inputHandler);
+    inputImg.addEventListener('input', inputHandler);
   });
 
   function inputHandler(e) {
     const file = e.target.files[0];
     const reader = new FileReader();
-    const pictureBoard = e.target.closest(".pictureBoard");
-    const img = pictureBoard.querySelector("img");
+    const pictureBoard = e.target.closest('.pictureBoard');
+    const img = pictureBoard.querySelector('img');
 
     // 이미지 업로드
     reader.readAsDataURL(file);
 
     reader.onload = function () {
-      img.setAttribute("src", this.result);
-      console.log(this); // this는 FileReader, resutl는 FileReader의 result로 이미지 url
+      img.setAttribute('src', this.result);
 
       // 삭제 버튼 생성
-      const btn = document.createElement("button");
+      const btn = document.createElement('button');
       pictureBoard.appendChild(btn);
 
       // 이미지 삭제하기
-      btn.addEventListener("click", function () {
-        img.removeAttribute("src");
+      btn.addEventListener('click', function () {
+        img.removeAttribute('src');
         pictureBoard.removeChild(btn);
       });
+
+      e.target.value = '';
     };
   }
 });
 
 // 날짜 찍기
 const date = new Date();
-const day = document.querySelector(".day");
+const day = document.querySelector('.day');
 
 let year = date.getFullYear();
 let month = date.getMonth() + 1;
@@ -42,7 +43,7 @@ let days = date.getDate();
 
 // '년-월-일' 형식으로 표시합니다.
 if (days < 10) {
-  days = "0" + days;
+  days = '0' + days;
 }
 const formattedDate = `${year}-${month}-${days}`;
 
@@ -76,8 +77,7 @@ publish.addEventListener('click', function () {
   const title = document.querySelector('.title');
   const Img = document.querySelectorAll('img');
   const content = document.querySelector('textarea');
-  const weather = document.querySelector('ul');
-  console.log(Img);
+  const weather = document.querySelector('.weatherDropToggle');
 
   const titleValue = title.value;
   const firstImgValue = Img[0].src;
@@ -85,7 +85,7 @@ publish.addEventListener('click', function () {
   const thirdImgValue = Img[2].src;
   const fourthImgValue = Img[3].src;
   const contentValue = content.value;
-  const weatherValue = weatherDropToggle.textContent;
+  const weatherValue = weather.textContent;
 
   //localStorage의 userName객체에서 loggedinUser를 가져와서 author라는 변수에 할당
   const author = localStorage.getItem('loggedinUser');
@@ -100,12 +100,12 @@ publish.addEventListener('click', function () {
     src4: fourthImgValue,
     num: Date.now(),
     day: formattedDate,
-    // weather: weatherValue,
+    weather: weatherValue,
   };
 
   // articleArray.push(thisArticle);
   //console.log(articleArray);
   let test = JSON.stringify(thisArticle);
   localStorage.setItem(`article${Date.now()}`, test);
-  location.href = "articles.html";
+  location.href = 'articles.html';
 });
